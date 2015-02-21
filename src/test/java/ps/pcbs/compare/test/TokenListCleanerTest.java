7a9776fc85f44d86c95cf8b53f1669067ca35b0a
karreem <karim.tachfint@gmail.com>
2015-02-21 10:24:08 +0100

    update tests according to new spec supplied mi-february

--- src/test/java/ps/pcbs/compare/test/TokenListCleanerTest.java
+++ src/test/java/ps/pcbs/compare/test/TokenListCleanerTest.java
@@ -16,13 +16,69 @@ public class TokenListCleanerTest {
 	public void testClean() {
 
 		TokenListCleaner cleaner = new TokenListCleaner();
-		cleaner.setToken("ه,رام الله,مكتب,شركة,دكتور,شركه,ة");
-		System.out.print(cleaner.getToken());
-		assertEquals("برستيج للاستثمار السياحي باسل",cleaner.clean("شركة برستيج للاستثمار السياحي باسل"));
 		
-		assertEquals(cleaner.clean("المركز الفني للصيانه"),cleaner.clean("المركز الفي للصيانة"));
+		//Step1
+		cleaner.setToken("رام ه,مكتب,شركة,دكتور,شركه,شراكة,شراكه,الشركة,الشركه,واخوانه,وإخوانه,واولاده,وأولاده,محلات,محل,ة,ه"); 
 
-		assertEquals("13", cleaner.clean("13دكتور"));
+
+		
+		assertEquals(cleaner.clean("and"),cleaner.clean("     _ *   + =  ) ( & ^ % $ # @ ! ~  : ;  ?> < } { ’ , . [ ] )"));
+		assertEquals(cleaner.clean("برستيج للاستثمار السياحي باسل"),cleaner.clean("شركة برستيج للاستثمار السياحي باسل"));
+		assertEquals(cleaner.clean(" \"مخمر موز\" "),cleaner.clean("مخمر موز"));
+		assertEquals(cleaner.clean("كيك  & بيك"), cleaner.clean("كيك  و بيك"));
+		assertEquals(cleaner.clean("art &  glft"), cleaner.clean("art and  glft"));
+		assertEquals(cleaner.clean(""),cleaner.clean(""));
+		assertEquals(cleaner.clean("المركز الفني للصيانه"),cleaner.clean("المركز الفني للصيانة"));
+		
+		assertEquals(cleaner.clean("كافتيريا الامانه"), cleaner.clean("كافتيريا الامانة"));
+		
+		assertEquals(cleaner.clean("تكسي المأمون "), cleaner.clean("تكسي المامون  "));
+		
+		assertEquals(cleaner.clean("شركه الإعتماد"), cleaner.clean("شركه الاعتماد"));
+		
+		assertEquals(cleaner.clean("الإعتماد"), cleaner.clean("شراكة الإعتماد شركه شركة شراكه"));
+		
+		
+		assertEquals(cleaner.clean("الإعتماد"), cleaner.clean("الإعتماد  محل"));
+	
+	
+		assertEquals(cleaner.clean("الإعتماد"), cleaner.clean("الإعتماد وإخوانه واولاده وأولاده محلات محل"));
+
+		assertEquals("", cleaner.clean("13دكتور"));
+		
+
+		
+		assertEquals(cleaner.clean("يور هوم 4"), cleaner.clean("فور يور هوم"));
+		
+		
+		assertEquals(cleaner.clean("4 me"), cleaner.clean("for me"));
+		
+		//Step2
+		assertEquals(cleaner.clean("ابو محمد"), cleaner.clean("ابومحمد"));
+		
+		assertEquals(cleaner.clean("أبو أحمد"), cleaner.clean("أبوأحمد"));
+		
+		assertEquals(cleaner.clean("عبد الكريم"), cleaner.clean("عبدالكريم"));
+		
+		assertEquals(cleaner.clean("دير ابوضعيف"), cleaner.clean("ديرابوضعيف"));
+		assertEquals(cleaner.clean("بير زيت"), cleaner.clean("بيرزيت"));
+		assertEquals(cleaner.clean("كفر عين"), cleaner.clean("كفر عين"));
+		assertEquals(cleaner.clean("عين يبرود"), cleaner.clean("عينيبرود"));
+		assertEquals(cleaner.clean("نيو ستايل"), cleaner.clean("نيوستايل"));
+		assertEquals(cleaner.clean("بيت لقيا"), cleaner.clean("بيتلقيا"));
+		
+		
+		
+		assertEquals(cleaner.clean("BLUE DRY CLEAN"), cleaner.clean("blue dry clean"));
+		
+		
+		//Step5
+		System.out.println(cleaner.clean("blue dry clean"));
+
+
+		
+		
+		
 //		cleaner = new TokenListCleaner(Arrays.asList("Chapi", "Chapo"));
 //		assertEquals("Patapo,   Patapi", cleaner.clean("Chapi Chapo Patapo, Chapo Chapi Patapi"));
 	}
