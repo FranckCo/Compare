55dd43f30aa84d8e5f05ee02d385a617dafe06b0
karreem <karim.tachfint@gmail.com>
2015-04-14 13:17:19 +0200

    new cases found in Hebron and some improvements to generalize phone cleaning

--- src/main/java/ps/pcbs/compare/duke/cleaners/PhoneCleaner.java
+++ src/main/java/ps/pcbs/compare/duke/cleaners/PhoneCleaner.java
@@ -20,18 +20,35 @@ public class PhoneCleaner implements Cleaner {
 
 	@Override
 	public String clean(String value) {
+		String compositeNumber;
+		
+		if(value.startsWith("02/")) value=value.replace("/", "");
+		
+		if(value.matches("([0-9]{7}|[0-9]{9}|[0-9]{10})\\s([0-9]{9}|[0-9]{10})")){
+			if(value.matches("([0-9]{10})\\s([0-9]{9}|[0-9]{10})")){
+			return value.substring(0,10)+Config.DEFAULT_TOKEN_SEPARATOR+value.substring(11);
+			}
+			if(value.matches("([0-9]{9})\\s([0-9]{9}|[0-9]{10})")){
+				return value.substring(0,9)+Config.DEFAULT_TOKEN_SEPARATOR+value.substring(10);
+				}
+			if(value.matches("([0-9]{7})\\s([0-9]{9}|[0-9]{10})")){
+				return "02"+value.substring(0,7)+Config.DEFAULT_TOKEN_SEPARATOR+value.substring(8);
+				}
+		}
 
 		String trimmed = value.trim().replaceAll(" ", "")
 				.replaceAll("\\p{InArabic}+", "");
-		String compositeNumber;
+		
 		Matcher matcher = pattern.matcher(trimmed);
-
+		
+		
+		
 		if (matcher.matches())
 			return "0" + trimmed.substring(0);
 
-		if (trimmed.matches("(23|24|26|27|28|29)[0-9]{5}")) {
+		if (trimmed.matches("(22|23|24|26|27|28|29)[0-9]{5}")) {
 			if (value.startsWith("23") | value.startsWith("27")
-					| value.startsWith("29")) {
+					| value.startsWith("29") | value.startsWith("22")) {
 				return "02" + trimmed;
 			}
 			if (value.startsWith("24"))
@@ -44,16 +61,15 @@ public class PhoneCleaner implements Cleaner {
 		}
 		if (trimmed.matches("29[0-9]{5}[^0-9]"))
 			return "02"
-					+ trimmed.replace("/", "").replace(".", "")
-							.replace("*", "").replace("-", "");
+					+ trimmed.replaceAll("[^0-9]","");
 
 		if (trimmed.matches("(23|24|27|29)[0-9]{5}[^0-9][0-9]")) {
 
 			if (value.startsWith("24")) {
-				compositeNumber = "04" + trimmed.substring(0, 7);
-				compositeNumber = compositeNumber
-						+ Config.DEFAULT_TOKEN_SEPARATOR
-						+ compositeNumber.substring(0, 8) + trimmed.charAt(8);
+				compositeNumber = "0" + trimmed.substring(0, 7)+ trimmed.charAt(8);
+//				"04"+  compositeNumber = compositeNumber
+//						+ Config.DEFAULT_TOKEN_SEPARATOR
+//						+ compositeNumber.substring(0, 8) + trimmed.charAt(8);
 
 				return compositeNumber;
 			}
@@ -95,17 +111,16 @@ public class PhoneCleaner implements Cleaner {
 			return "0" + trimmed.substring(1);
 
 		if (trimmed.matches("(02)[^0-9][0-9]{7}"))
-			return "02" + trimmed.substring(2).replace("-", "");
+			return "02" + trimmed.substring(2).replaceAll("[^0-9]","");
 
 		if (trimmed.matches("(0599)[^0-9][0-9]{6}"))
-			return "05" + trimmed.substring(2).replace("-", "");
+			return trimmed.replaceAll("[^0-9]","");
 
 		if (trimmed.matches("(05990599)[0-9]{6}"))
 			return trimmed.substring(4);
 
 		if (trimmed.matches("[0-9]{10}[^0-9]"))
-			return trimmed.replace("/", "").replace(".", "").replace("*", "")
-					.replace("-", "");
+			return trimmed.replaceAll("[^0-9]","");
 		;
 
 		if (trimmed.length() == 11) {
