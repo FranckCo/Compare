dbcef3e80cd92ff0014a1ef3f956e61cd0e9f04c
karreem <karim.tachfint@gmail.com>
2015-04-15 17:02:20 +0200

    add cases supplied in the new cleaning specification

--- src/test/java/ps/pcbs/compare/test/ArabicTextCleanerTest.java
+++ src/test/java/ps/pcbs/compare/test/ArabicTextCleanerTest.java
@@ -19,10 +19,46 @@ public class ArabicTextCleanerTest {
 
 	@Test
 	public void testClean() {
-		assertEquals(cleaner.clean("كافتيريا الامانة"), "كافتيريا الامانه");
-		assertEquals(cleaner.clean("تكسي المأمون"), "تكسي المامون");
-		assertEquals(cleaner.clean("تكسي الإعتماد"), "تكسي الاعتماد");
-		assertEquals(cleaner.clean("تكسي الإعتماد"), "تكسي الاعتماد");
+		assertEquals(cleaner.clean("and"),cleaner.clean("     _ *   + =  ) ( & ^ % $ # @ ! ~  : ;  ?> < } { ’ , . [ ] )"));
+		assertEquals(cleaner.clean(" \"مخمر موز\" "),cleaner.clean("مخمر موز"));
+		assertEquals(cleaner.clean("كيك  & بيك"), cleaner.clean("كيك  و بيك"));
+		assertEquals(cleaner.clean("art &  glft"), cleaner.clean("art and  glft"));
+		assertEquals(cleaner.clean(""),cleaner.clean(""));
+		assertEquals(cleaner.clean("المركز الفني للصيانه"),cleaner.clean("المركز الفني للصيانة"));
+		
+		assertEquals(cleaner.clean("كافتيريا الامانه"), cleaner.clean("كافتيريا الامانة"));
+		
+		assertEquals(cleaner.clean("تكسي المأمون "), cleaner.clean("تكسي المامون  "));
+		
+		assertEquals(cleaner.clean("شركه الإعتماد"), cleaner.clean("شركه الاعتماد"));
+		
+		assertEquals(cleaner.clean("يور هوم 4"), cleaner.clean("فور يور هوم"));
+		
+		
+		assertEquals(cleaner.clean("4 me"), cleaner.clean("for me"));
+		
+		//Step2
+		assertEquals(cleaner.clean("ابو محمد"), cleaner.clean("ابومحمد"));
+		
+		assertEquals(cleaner.clean("أبو أحمد"), cleaner.clean("أبوأحمد"));
+		
+		assertEquals(cleaner.clean("عبد الكريم"), cleaner.clean("عبدالكريم"));
+		
+		assertEquals(cleaner.clean("دير ابوضعيف"), cleaner.clean("ديرابوضعيف"));
+		assertEquals(cleaner.clean("بير زيت"), cleaner.clean("بيرزيت"));
+		assertEquals(cleaner.clean("كفر عين"), cleaner.clean("كفر عين"));
+		assertEquals(cleaner.clean("عين يبرود"), cleaner.clean("عينيبرود"));
+		assertEquals(cleaner.clean("نيو ستايل"), cleaner.clean("نيوستايل"));
+		assertEquals(cleaner.clean("بيت لقيا"), cleaner.clean("بيتلقيا"));
+		
+		
+		
+		assertEquals(cleaner.clean("BLUE DRY CLEAN"), cleaner.clean("blue dry clean"));
+		
+		
+		//Step5
+		System.out.println(cleaner.clean("blue dry clean"));
+
 	}
 
 }
