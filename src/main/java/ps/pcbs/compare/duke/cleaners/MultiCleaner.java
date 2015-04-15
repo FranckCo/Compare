9d17b8d4a742f811bfd9ad50c474f11a1e3cf1ec
karreem <karim.tachfint@gmail.com>
2015-04-15 16:55:11 +0200

    code refactoring

--- src/main/java/ps/pcbs/compare/duke/cleaners/MultiCleaner.java
+++ src/main/java/ps/pcbs/compare/duke/cleaners/MultiCleaner.java
@@ -1,20 +1,22 @@
 package ps.pcbs.compare.duke.cleaners;
 
+import ps.pcbs.compare.Config;
 import no.priv.garshol.duke.Cleaner;
 
 public class MultiCleaner<T extends Cleaner> implements Cleaner {
 
 	private String baseCleaner = null;
-	private String separator = null;
+	private String separator = Config.DEFAULT_TOKEN_SEPARATOR;
 	private Cleaner Cleaner = null;
 
 	@Override
 	public String clean(String value) {
 
 		String[] tokens = value.split(separator);
+
 		StringBuilder builder = new StringBuilder();
 		 for (String token : tokens)
-		 builder.append(this.getCleaner().clean(token)).append(separator);
+		 builder.append(this.getCleaner().clean(token)).append(this.getSeparator());
 		if(builder.length()>0){
 		if (builder.substring(builder.length() - 1).equals(separator))
 			builder.deleteCharAt(builder.length() - 1);
@@ -32,19 +34,19 @@ public class MultiCleaner<T extends Cleaner> implements Cleaner {
 	}
 
 	public Cleaner getCleaner() {
-		try {
-			Class<?> cleanerClass = Class.forName(this.baseCleaner);
-			this.Cleaner = (Cleaner) cleanerClass.newInstance();
-		} catch (Exception e) {
-			throw new IllegalArgumentException("Invalid class name "
-					+ baseCleaner);
-		}
+//		try {
+//			Class<?> cleanerClass = Class.forName(this.baseCleaner);
+//			this.Cleaner = (Cleaner) cleanerClass.newInstance();
+//		} catch (Exception e) {
+//			throw new IllegalArgumentException("Invalid class name "
+//					+ baseCleaner);
+//		}
 		return Cleaner;
 	}
 
 	
 	 public void setCleaner(Cleaner cleaner) {
-	 Cleaner = cleaner;
+	 this.Cleaner = cleaner;
 	 }
 
 	public void setBaseCleaner(String baseCleaner) {
